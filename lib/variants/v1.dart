050dc3023c33a5f82397d5631a1342311ffb5e97
Murodjon Erkinov <murodjonerkinov2005@gmail.com>
2024-10-01 21:46:52 -0700

    first commit

---
+++ lib/variants/v1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class V1 extends StatelessWidget {
+  const V1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogo();
+  }
+}
